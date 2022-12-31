7b9480af7886a15d648c741bb39d789bff837ce3
Default Cyberid <defaultcyberid+dev@outlook.com>
2022-12-31 21:26:30 +0800

    update

---
+++ powershell/user_profile.ps1
@@ -0,0 +1,26 @@
+# Prompt
+function Get-ScriptDirectory { Split-Path $MyInvocation.ScriptName }
+$PROMPT_CONFIG = Join-Path (Get-ScriptDirectory) 'default.omp.json'
+oh-my-posh --init --shell pwsh --config $PROMPT_CONFIG | Invoke-Expression
+
+# Terminal Icons
+Import-Module Terminal-Icons
+
+# PSReadline
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadlineOption -PredictionViewStyle ListView
+
+# PSFzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Which command
+function which ($command) {
+    Get-Command -Name $command -ErrorAction SilentlyContinue |
+        Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+
+# Alias
+Set-Alias -Name ll -Value ls
